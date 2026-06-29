116618dcac51a329d87011020f26ca5969c3caff
Hawra-14 <hawraaabdulla535@gmail.com>
2026-06-29 15:04:43 +0300

    add counters to brdRow and sqr

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const kb = document.querySelector('.keyboard')
 const key = document.querySelector('.key')
+const brdRow = document.querySelectorAll('.board-row')
+const sqr = document.querySelectorAll('.sqr')
+const board = document.querySelectorAll('.board')
+
+// console.log(brdRow[0].children[0]) 
+// console.log(sqr)
 
 let currentGuess = []
-const word = ['b','u','i','l','d']
-// console.log(word.length)
+const word = ['b', 'u', 'i', 'l', 'd']
+let enterbtn = false
+let brdRowCounter = 0
+let sqrCounter = 0
 
 kb.addEventListener('click', function (event) {
     // console.log(event.target)
@@ -16,26 +24,60 @@ kb.addEventListener('click', function (event) {
 
     if (label === 'ENTER') {
         if (currentGuess.length === 5) {
-            // check the matches
+            enterbtn = true
             console.log('Enter')
+            // check the matches
+            brdRowCounter++
+            console.log(brdRowCounter);
+
         }
         else {
-            return 'Not enough letters'
+            console.log('Not enough letters')
         }
     }
     else if (label === 'DELETE') {
-        // currentGuess = currentGuess.length - 1 // it stores the length to the currentGuess as a number
-        currentGuess.slice(currentGuess.length, 1)
+        // currentGuess = currentGuess.length - 1 // it stores the length to the currentGuess as a number!
+        currentGuess = currentGuess.slice(0, -1) // Delete the last letter on the currentGuess
         console.log('Delete')
         console.log(currentGuess)
+        brdRow[brdRowCounter].children[sqrCounter].textContent = ''
     }
     else {
         if (currentGuess.length === 5) {
-            key.classList.add('.disable')
+            // key.classList.add('.disable')
+            // return
         }
         else {
-            currentGuess += label
-            console.log(currentGuess)
+            console.log(brdRow[brdRowCounter])
+            console.log(sqr[sqrCounter])
+
+            if (brdRow[brdRowCounter].sqr[sqrCounter].textContent === '') {
+                currentGuess.push(label)
+                console.log(currentGuess)
+                brdRow[brdRowCounter].children[sqrCounter].textContent = label
+                sqrCounter++
+                console.log(brdRow[brdRowCounter]);
+                
+            }
+
         }
+        // for (i = 0; i < board.length; i++) {
+        //     for (j = 0; j < brdRow.length; j++) {
+        //         if (currentGuess.length === 5) {
+        //             key.classList.add('.disable')
+        //             return
+        //         }
+        //         else {
+        //             currentGuess = currentGuess.push(label)
+        //             // brdRow[i].children[j].textContent = label
+        //         }
+        //     }
+        // }
     }
 })
+
+
+// we have an array that represents the board
+// user clicks button --> textContent is pushed into array
+// index value of pushed letter is also the id of the square that needs updated
+// update the square textContent with letter from array
