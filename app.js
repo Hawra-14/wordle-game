21a03e1fe119452f3baff0f0b2a5ceea738ae9b8
Hawra-14 <hawraaabdulla535@gmail.com>
2026-06-30 19:07:35 +0300

    Add if statement that checks is whole currentGuess's array is correct to print win message

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const kb = document.querySelector('.keyboard')
+const key = document.querySelectorAll('.key')
 const brdRow = document.querySelectorAll('.board-row')
 const sqr = document.querySelectorAll('.sqr')
 const board = document.querySelectorAll('.board')
@@ -8,15 +9,19 @@ const message = document.querySelector('.message')
 // console.log(sqr)
 
 let currentGuess = []
-const word = ['B', 'U', 'I', 'L', 'D']
+const word = ['R', 'E', 'A', 'D', 'Y']
 let brdRowCounter = 0
 let sqrCounter = 0
-let allCorrect = true
+let allCorrect
+let keyClicked = []
+
 
 kb.addEventListener('click', function (event) {
     // console.log(event.target)
     const clickedKey = event.target // This give the whole button tag
     const label = clickedKey.textContent // To store the content
+    keyClicked.push(clickedKey)
+    console.log(keyClicked)
 
     if (!event.target.classList.contains('key')) { // Allow clicking on key class only 
         return
@@ -47,29 +52,35 @@ kb.addEventListener('click', function (event) {
 function enterClicked() {
     if (currentGuess.length === 5) {
         console.log('Enter')
+        console.log(keyClicked)
         for (let i = 0; i < 5; i++) {
             allCorrect = true
             const square = brdRow[brdRowCounter].children[i]
-            if (currentGuess[i] === word[i]) {
+            if (JSON.stringify(currentGuess) === JSON.stringify(word)) {
                 square.style.backgroundColor = 'green'
             }
+            else if (currentGuess[i] === word[i]) {
+                square.style.backgroundColor = 'green'
+                // keyClicked.style.backgroundColor = 'green'
+                allCorrect = false
+            }
             else if (word.includes(currentGuess[i])) {
-                square.style.backgroundColor = 'yellow'
+                square.style.backgroundColor = '#b59f3b'
                 allCorrect = false
             }
             else {
                 square.style.backgroundColor = 'grey'
                 allCorrect = false
             }
+            square.style.color = 'white'
         }
-
         if (allCorrect) {
             kb.style.pointerEvents = 'none'
-            message.textContent = 'You Win 🎉'
+            message.textContent = 'CORRECT 🎉'
         }
-        else if (allCorrect === false && brdRow[5].children[4].textContent !== ''){
+        else if (allCorrect === false && brdRow[5].children[4].textContent !== '') {
             kb.style.pointerEvents = 'none'
-
+            message.textContent = `NOO, YOU LOSS 👎🏼 \n IT WAS ${word.join('')}`
         }
 
         // if (word[0] === currentGuess[0]) {
